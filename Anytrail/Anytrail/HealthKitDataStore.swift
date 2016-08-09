8b8d7c279a1ae2593a184cfe1bf0dab4f23c4954
Cordavi <michaelamundsen@gmail.com>
2016-08-09 12:11:52 -0400

    Implmented getSamplesWithData function so you can query for a healthkit data type sample within a given data range and it will return the data results

--- Anytrail/Anytrail/HealthKitDataStore.swift
+++ Anytrail/Anytrail/HealthKitDataStore.swift
@@ -11,7 +11,29 @@ import HealthKit
 
 class HealthKitDataStore {
     
+    typealias healthKitSamplesData = (dataSamples: [HKQuantitySample], error: NSError?)
     static let healthKitStore = HKHealthStore()
     
-
+    class func getSampleDataWithInDates(sampleType:HKSampleType, startDate: NSDate, endDate: NSDate, limit: Int, ascendingValue: Bool, completion: healthKitSamplesData -> Void) {
+        
+        let dateRangePredicate = HKQuery.predicateForSamplesWithStartDate(startDate, endDate: endDate, options: .None)
+        
+        let sortDescriptor = NSSortDescriptor(key:HKSampleSortIdentifierStartDate, ascending: ascendingValue)
+        
+        let sampleQuery = HKSampleQuery(sampleType: sampleType, predicate: dateRangePredicate, limit: limit, sortDescriptors: [sortDescriptor]) { (sampleQuery, results, error ) -> Void in
+            
+            guard let results = results where error == nil else {
+                print(error)
+                completion((dataSamples: [], error: error))
+                return
+            }
+            
+            let samplesArray = results as? [HKQuantitySample]
+            
+            if let samplesArray = samplesArray {
+                completion((dataSamples: samplesArray, error: nil))
+            }
+        }
+        healthKitStore.executeQuery(sampleQuery)
+    }
 }
