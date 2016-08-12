5cac0a6ec6b3fe2c6d65f115ce94d2c46cc49d33
Cordavi <michaelamundsen@gmail.com>
2016-08-12 13:42:24 -0400

    Adding temporary viewcontroller to be used for walk session tracking

---
+++ Anytrail/Anytrail/Controllers/WalkTrackerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WalkTrackerViewController.swift
+//  Anytrail
+//
+//  Created by Michael Amundsen on 8/12/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class WalkTrackerViewController: UIViewController {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func startTapped(sender: AnyObject) {
+        
+    }
+
+    @IBAction func stopTapped(sender: AnyObject) {
+        
+    }
+
+
+}
