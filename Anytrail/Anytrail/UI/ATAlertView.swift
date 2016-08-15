bf45593be6a4251a2ee1f8ea29a30b053047ee61
imryan <notryancohen@gmail.com>
2016-08-15 17:37:32 -0400

    current location pin colors, POI colors, selected waypoint colors. Lots of colors

--- Anytrail/Anytrail/UI/ATAlertView.swift
+++ Anytrail/Anytrail/UI/ATAlertView.swift
@@ -11,11 +11,11 @@ import JSSAlertView
 
 class ATAlertView {
     
-    struct ATAlertViewColor {
-        static let GREY = UIColorFromHex(0xE3E7EA, alpha: 0.9)
-        static let RED = UIColorFromHex(0xE74C3C, alpha: 0.9)
-        static let GREEN = UIColorFromHex(0x27AE60, alpha: 0.9)
-    }
+//    struct ATAlertViewColor {
+//        static let GREY = UIColorFromHex(0xE3E7EA, alpha: 0.9)
+//        static let RED = UIColorFromHex(0xE74C3C, alpha: 0.9)
+//        static let GREEN = UIColorFromHex(0x27AE60, alpha: 0.9)
+//    }
     
     internal enum ATAlertViewType: Int {
         case Normal = 1
@@ -27,11 +27,11 @@ class ATAlertView {
         let color: UIColor
         
         if type == .Normal {
-            color = ATAlertViewColor.GREY
+            color = ATConstants.Colors.GRAY
         } else if type == .Error {
-            color = ATAlertViewColor.RED
+            color = ATConstants.Colors.RED
         } else {
-            color = ATAlertViewColor.GREEN
+            color = ATConstants.Colors.GREEN
         }
         
         let alert = JSSAlertView().show(
