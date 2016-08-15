1a3a4cf1937633537897a5539ce83462aeda88eb
Uskinn <s.nevzor@yahoo.com>
2016-08-15 14:42:14 -0400

    My Paths hardcoded but it works

--- Anytrail/Anytrail/ATPathViewController.swift
+++ Anytrail/Anytrail/ATPathViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ATPathViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var images = [UIImage(named: "path1"), UIImage(named: "path2"), UIImage(named: "path3"), UIImage(named: "path4"), UIImage(named: "path5")]
+    var date = ["June 12, 2016", "July 8, 2016", "August 2, 2016", "August 19, 2016", "August 23, 2016"]
+    var location = ["New York", "Boston", "Bennington", "Cape May", "Chicago"]
+    var distance = ["2.5 mi", "4.8 mi", "9.3 mi", "6.1 mi", "12.4"]
+    var steps = ["5.934", "12.372", "24.153", "14.942", "28.337"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,13 +31,18 @@ class ATPathViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+       return 5
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! ATPathCell
         
-        
+        cell.mapImage.image = images[indexPath.row]
+        cell.dateLabel.text = date[indexPath.row]
+        cell.locationLabel.text = location[indexPath.row]
+        cell.distanceLabel.text = distance[indexPath.row]
+        cell.stepsLabel.text = steps[indexPath.row]
         
         
         return cell
