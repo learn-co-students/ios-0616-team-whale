fd4cf6a0460e8800e996cbf966fdc54237012421
Cordavi <michaelamundsen@gmail.com>
2016-08-15 14:29:39 -0400

    Added convience init to restart walk with time and distance activity between when the app closed and reopenned

--- Anytrail/Anytrail/WalkTracker.swift
+++ Anytrail/Anytrail/WalkTracker.swift
@@ -20,6 +20,28 @@ class WalkTracker: NSObject {
     
     static let walkTrackerSharedSession = WalkTracker()
     
+    convenience init(startDate: NSDate, continueDate: NSDate) {
+        self.walkStartDate = startDate
+        self.walkEndDate = continueDate
+        self.currentWalkTime = secondsBetweenDates(startDate, endDate: continueDate)
+        self.walkDistance = getWalkDistance(startDate, endDate: continueDate)
+    }
+    
+    func secondsBetweenDates(startDate: NSDate, endDate: NSDate) -> Int {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = calendar.components([.Second], fromDate: startDate, toDate: endDate, options: [])
+        
+        return components.second
+    }
+    
+    func getWalkDistance(startDate: NSDate, endDate: NSDate) -> Int {
+        HealthKitDataStore.sharedInstance.getSampleDataWithInDates(HealthKitDataTypes.walkingRunningDistance!, startDate: startDate, endDate: endDate, limit: 0, ascendingValue: true) { distanceData in
+            let distanceSamples = distanceData.dataSamples
+            distanceSamples.reduce(0) { $0 + $1.quantity }
+        }
+    }
+    
     func startWalk() {
         walkTimer = NSTimer.scheduledTimerWithTimeInterval(1,
                                                            target: self,
@@ -53,7 +75,6 @@ class WalkTracker: NSObject {
         walkEndDate = NSDate()
         pedometer.stopPedometerUpdates()
         walkTimer.invalidate()
-
         HealthKitDataStore.sharedInstance.saveWalk(walkDistance, timeRecorded: currentWalkTime, startDate: walkStartDate, endDate: walkEndDate)
     }
 }
