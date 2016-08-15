555a31054e393afa63248c4f5b1e4c13c6c3e36b
ellscha <escharlin@wesleyan.edu>
2016-08-15 10:09:25 -0400

    going to profile

---
+++ Anytrail/Anytrail/something.swift
@@ -0,0 +1,13 @@
+//
+//  something.swift
+//  Anytrail
+//
+//  Created by Elli Scharlin on 8/14/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Cocoa
+
+class something: UITableViewController {
+
+}
