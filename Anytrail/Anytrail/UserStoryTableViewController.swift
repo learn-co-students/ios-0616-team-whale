fb67b28b6ad7cf0f323ccda80a92614179b08ebb
ellscha <escharlin@wesleyan.edu>
2016-08-15 10:09:42 -0400

    so I can push UA

--- Anytrail/Anytrail/UserStoryTableViewController.swift
+++ Anytrail/Anytrail/UserStoryTableViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 import Mapbox
 
 class UserStoryTableViewController: UITableViewController {
-    @IBOutlet weak var distanceTravelledCell: UITableViewCell!
-    @IBOutlet weak var stepsTakenCell: UITableViewCell!
-    @IBOutlet weak var flightsClimbedCell: UITableViewCell!
+
+    let hkStore = HealthKitDataStore.sharedInstance
+    @IBOutlet weak var iconForCell: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +23,48 @@ class UserStoryTableViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var reuseIdentifier:String = ""
+        switch indexPath {
+        case 1:
+            reuseIdentifier = "stepsTaken"
+        case 2:
+            reuseIdentifier = "flightsClimbed"
+        case 3:
+            reuseIdentifier = "distanceTravelled"
+        default:
+            reuseIdentifier = "mapCell"
+        }
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath)
+    
+        switch reuseIdentifier {
+        case "stepsTaken":
+//            cell.textLabel?.text = hkStore.getSampleDataWithInDates(HealthKitDataTypes.stepCount, startDate: NSDate.init(), endDate: NSDate.distantFuture(), limit: , ascendingValue: <#T##Bool#>, completion: { (<#healthKitSamplesData#>) in
+        case "flightsClimbed":
+        case "distanceTravelled":
+            
+        default:
+            //"mapCell"
+        
+        }
+
+        //        let data : FoursquareData = self.store.data[indexPath.row]
+        //        cell.textLabel?.text = data.placeName
+        
+        
+        
+        return cell
     }
     
 //    func updateHealthKitData
