f3514d0037a24b33fe17f076b339b54731873d70
Uskinn <s.nevzor@yahoo.com>
2016-08-17 12:03:03 -0400

    Splash screen done

--- Anytrail/Anytrail/ATSplashScreenController.swift
+++ Anytrail/Anytrail/ATSplashScreenController.swift
@@ -19,20 +19,16 @@ class ATSplashScreenController: UIViewController {
         super.viewDidLoad()
         
         self.mask = CALayer()
-        self.mask?.contents = UIImage(named: "shoePrint")!.CGImage
+        self.mask?.contents = UIImage(named: "liberty")!.CGImage
         self.mask?.contentsGravity = kCAGravityResizeAspect
-        self.mask?.bounds = CGRect(x: 0, y: 0, width: 100, height: 100)
+        self.mask?.bounds = CGRect(x: 0, y: 0, width: 220, height: 220)
         self.mask?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
         self.mask?.position = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height/2)
-        
         whaleImageView.layer.mask = mask
-        //self.view.backgroundColor = UIColor(red: 107/255.0, green: 176/255.0, blue: 62/255.0, alpha: 1)
-      
-//        self.view.backgroundColor = UIColor(red: 41/255.0, green: 111/255.0, blue: 126/255.0, alpha: 1)
-        self.view.backgroundColor = UIColor(red: 32/255.0, green: 32/255.0, blue: 32/255.0, alpha: 0.9)
-        
+        self.view.backgroundColor = UIColor(red: 89/255.0, green: 90/255.0, blue: 92/255.0, alpha: 0.9)
+
         animate()
-        
     }
     
     func animate() {
@@ -41,15 +37,14 @@ class ATSplashScreenController: UIViewController {
         keyFrameAnimation.duration = 1
         keyFrameAnimation.beginTime = CACurrentMediaTime() + 1
         
-        let initialBounds = NSValue(CGRect: mask!.bounds)
+        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
         
+        let initialBounds = NSValue(CGRect: mask!.bounds)
         let middleBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 90, height: 90))
-        let finalBounds =  NSValue(CGRect: CGRect(x: 0, y: 0, width: 1500, height: 1500))
+        let finalBounds =  NSValue(CGRect: CGRect(x: 0, y: 0, width: 3000, height: 3000))
         
         keyFrameAnimation.values = [initialBounds, middleBounds, finalBounds]
-        keyFrameAnimation.keyTimes = [0, 0.3, 1.8]
-        
-        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+        keyFrameAnimation.keyTimes = [0, 0.3, 1.1]
         
         self.mask?.addAnimation(keyFrameAnimation, forKey: "bounds")
     }
@@ -57,21 +52,4 @@ class ATSplashScreenController: UIViewController {
     override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
         self.whaleImageView.layer.mask = nil
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
