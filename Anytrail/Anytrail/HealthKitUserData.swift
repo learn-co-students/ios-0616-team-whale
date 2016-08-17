7d5ab7c70e1f3c0ace8a20cc7bbc22ca0ee27e42
Cordavi <michaelamundsen@gmail.com>
2016-08-17 09:05:45 -0400

    Added functions to get sum of healthkit data types within the last 24 hour period

--- Anytrail/Anytrail/HealthKitUserData.swift
+++ Anytrail/Anytrail/HealthKitUserData.swift
@@ -13,7 +13,7 @@ class HealthKitUserData {
     
     let userHealthData: [String]
     let today = NSDate()
-    let yesterday = NSCalendar.currentCalendar().dateByAddingUnit(.Day, value: -1, toDate: NSDate(), options: [])
+    let yesterday = NSCalendar.currentCalendar().dateByAddingUnit(.Day, value: -1, toDate: NSDate(), options: []) ?? NSDate()
     
     var todayStepCount: Double?
     var todayBasalEnergy: Double?
@@ -29,46 +29,65 @@ class HealthKitUserData {
     }
     
     func getStepCountForToday(completion: (Double?) -> Void) {
-        
-        
-        HealthKitDataStore.sharedInstance.sumOfData(HealthKitDataStoreSampleTypes.stepCount!, fromDate: yesterday!, toDate: today, statisticOptions: .CumulativeSum, unitType: HKUnit.countUnit()) { sumData in
-            completion(sumData.value)
+        guard let stepCountType = HealthKitDataStoreSampleTypes.stepCount else {
+            return
+        }
+        HealthKitDataStore.sharedInstance.sumOfData(stepCountType, fromDate: yesterday, toDate: today, statisticOptions: .CumulativeSum, unitType: HKUnit.countUnit()) { stepCountSum in
+            completion(stepCountSum.value)
         }
     }
     
-    //    func getBasalEnergyForToday() {
-    //        HealthKitDataStore.sharedInstance.healthKitStatisticQueryWithCompletionHandler(HealthKitDataTypes.basalEnergyBurned!, startDate: yesterday!, endDate: today, statisticResultType: "sum", HKUnitType: HKUnit.calorieUnit()) { healthKitStatisticData in
-    //            self.todayBasalEnergy = healthKitStatisticData.statisticValue
-    //        }
-    //    }
-    //
-    //    func getFlightCountForToday() {
-    //        HealthKitDataStore.sharedInstance.healthKitStatisticQueryWithCompletionHandler(HealthKitDataTypes.flightsClimbed!, startDate: yesterday!, endDate: today, statisticResultType: "sum", HKUnitType: HKUnit.countUnit()) { healthKitStatisticData in
-    //            self.todayFlightClimbed = healthKitStatisticData.statisticValue
-    //        }
-    //    }
-    //
-    //    func getDistanceForToday() {
-    //        HealthKitDataStore.sharedInstance.healthKitStatisticQueryWithCompletionHandler(HealthKitDataTypes.walkingRunningDistance!, startDate: yesterday!, endDate: today, statisticResultType: "sum", HKUnitType: HKUnit.meterUnit()) { healthKitStatisticData in
-    //            self.todayDistance = healthKitStatisticData.statisticValue
-    //        }
-    //    }
-    //
-    //    func getExerciseForToday() {
-    //        HealthKitDataStore.sharedInstance.healthKitStatisticQueryWithCompletionHandler(HealthKitDataTypes.exerciseTime!, startDate: yesterday!, endDate: today, statisticResultType: "sum", HKUnitType: HKUnit.minuteUnit()) { healthKitStatisticData in
-    //            self.todayExercise = healthKitStatisticData.statisticValue
-    //        }
-    //    }
-    //
-    //    func getActiveEnergyForToday() {
-    //        HealthKitDataStore.sharedInstance.healthKitStatisticQueryWithCompletionHandler(HealthKitDataTypes.activeEnergyBurned!, startDate: yesterday!, endDate: today, statisticResultType: "sum", HKUnitType: HKUnit.calorieUnit()) { healthKitStatisticData in
-    //            self.todayActiveEnergy = healthKitStatisticData.statisticValue
-    //        }
-    //    }
-    //
-    //    func getWaterConsumptionToday() {
-    //        HealthKitDataStore.sharedInstance.healthKitStatisticQueryWithCompletionHandler(HealthKitDataTypes.waterConsumption!, startDate: yesterday!, endDate: today, statisticResultType: "sum", HKUnitType: HKUnit.literUnit()) { healthKitStatisticData in
-    //            self.todayWaterCosumed = healthKitStatisticData.statisticValue
-    //        }
-    //    }
+    func getBasalEnergyForToday(completion: (Double?) -> Void) {
+        guard let basalEnergyType = HealthKitDataStoreSampleTypes.basalEnergyBurned else {
+            return
+        }
+        HealthKitDataStore.sharedInstance.sumOfData(basalEnergyType, fromDate: yesterday, toDate: today, statisticOptions: .CumulativeSum, unitType: HKUnit.calorieUnit()) { basalEnergySum in
+            completion(basalEnergySum.value)
+        }
+    }
+    
+    func getFlightCountForToday(completion: (Double?) -> Void) {
+        guard let flightClimbedType = HealthKitDataStoreSampleTypes.flightsClimbed else {
+            return
+        }
+        HealthKitDataStore.sharedInstance.sumOfData(flightClimbedType, fromDate: yesterday, toDate: today, statisticOptions: .CumulativeSum, unitType: HKUnit.countUnit()) { flightClimbedSum in
+            completion(flightClimbedSum.value)
+        }
+    }
+    
+    func getDistanceForToday(completion: (Double?) -> Void) {
+        guard let distanceWalkedType = HealthKitDataStoreSampleTypes.walkingRunningDistance else {
+            return
+        }
+        HealthKitDataStore.sharedInstance.sumOfData(distanceWalkedType, fromDate: yesterday, toDate: today, statisticOptions: .CumulativeSum, unitType: HKUnit.meterUnit()) { distanceWalkedSum in
+            completion(distanceWalkedSum.value)
+        }
+    }
+    
+    func getExerciseForToday(completion: (Double?) -> Void) {
+        guard let exerciseTimeType = HealthKitDataStoreSampleTypes.exerciseTime else {
+            return
+        }
+        HealthKitDataStore.sharedInstance.sumOfData(exerciseTimeType, fromDate: yesterday, toDate: today, statisticOptions: .CumulativeSum, unitType: HKUnit.minuteUnit()) { exerciseTimeSum in
+            completion(exerciseTimeSum.value)
+        }
+    }
+    
+    func getActiveEnergyForToday(completion: (Double?) -> Void) {
+        guard let activeEnergyType = HealthKitDataStoreSampleTypes.activeEnergyBurned else {
+            return
+        }
+        HealthKitDataStore.sharedInstance.sumOfData(activeEnergyType, fromDate: yesterday, toDate: today, statisticOptions: .CumulativeSum, unitType: HKUnit.calorieUnit()) { activeEnergySum in
+            completion(activeEnergySum.value)
+        }
+    }
+    
+    func getWaterConsumptionToday(completion: (Double?) -> Void) {
+        guard let waterConsumedType = HealthKitDataStoreSampleTypes.waterConsumption else {
+            return
+        }
+        HealthKitDataStore.sharedInstance.sumOfData(waterConsumedType, fromDate: yesterday, toDate: today, statisticOptions: .CumulativeSum, unitType: HKUnit.literUnit()) { waterConsumedSum in
+            completion(waterConsumedSum.value)
+        }
+    }
 }
