8eae45146e7b7fc8715ebefad842b08b26a465ad
Cordavi <michaelamundsen@gmail.com>
2016-08-20 09:41:55 -0400

    Changed foursquareData to set so there won't be duplicated. Moved api parameter creation to function that completes before api call is made with it. Generate FoursquareData with json data recieved back from the api call after completetion

--- Anytrail/Anytrail/ApisDataStore.swift
+++ Anytrail/Anytrail/ApisDataStore.swift
@@ -8,58 +8,78 @@
 
 import UIKit
 import SwiftyJSON
+import CoreLocation
 
 class ApisDataStore {
     
     static let sharedInstance = ApisDataStore()
-    
-    private init() {
-        //
-    }
-    
-    var foursquareData: [FoursquareData] = []
+    var foursquareData: Set<FoursquareData> = []
     var mashapeData: [MashapeData] = []
     var foursquareIDs: [String] = []
-
+    var foursquareParameters: [String: String] = [:]
+    
     
     func getDataWithCompletion(completion: () -> ()) {
-        FoursquareAPIClient.getQueryForSearchLandmarks { (json) in
-            self.foursquareData.removeAll()
-            guard let json = json else { print("error: no data recieved from API Client"); return }
-            
-            for object in json {
-                if let dataFS = object.1["groups"][0]["items"].array {
-                    
-                    for i in 0..<dataFS.count {
-                        self.foursquareData.append(FoursquareData(json: dataFS[i]))
-                    }
-                }
+        
+        if let originLocation = LocationDataStore.sharedInstance.origin,
+            destinationLocation = LocationDataStore.sharedInstance.destination {
+            foursquareParameters = prepareForLandmarksQuery(originLocation, destinationLocation: destinationLocation)
+        }
+        
+        FoursquareAPIClient.getQueryForSearchLandmarks(foursquareParameters) { itemsJSON in
+            guard let itemsArray = itemsJSON else {
+                print("error: no data recieved from API Client")
+                return
             }
+            
+            self.foursquareData.removeAll()
+            
+            for venue in itemsArray {
+                self.foursquareData.insert(FoursquareData(json: venue))
+            }
+            
             completion()
         }
     }
     
+    func prepareForLandmarksQuery(originLocation: CLLocationCoordinate2D, destinationLocation: CLLocationCoordinate2D) -> [String: String] {
+        
+        let startLocation = CLLocation(latitude: originLocation.latitude, longitude: originLocation.longitude)
+        let endLocation = CLLocation(latitude: destinationLocation.latitude, longitude: destinationLocation.longitude)
+        let midPoint = startLocation.distanceFromLocation(endLocation) / 2
+        let origin = "\(startLocation.coordinate.latitude), \(endLocation.coordinate.longitude)"
+        let parameter = ["client_id": Keys.fourSquareClientID,
+                         "client_secret": Keys.fourSquareClientSecret,
+                         "v": FoursquareConstants.v,
+                         "ll": origin,
+                         "query": FoursquareConstants.query,
+                         "radius": midPoint.description]
+        
+        return parameter
+    }
+}
+
 //    func getPhotoIDDataWithCompletion(completion: () -> ()) {
 //        FoursquareAPIClient.getPhotoForVenue(<#T##venueIdentifier: String##String#>, completion: <#T##(UIImage?) -> ()#>)
 //    }
-    
-    
-    func getTrailsWithCompletion(completion: () -> ()) {
-        MashapeAPIClient.getTrails { (json) in
-            self.mashapeData.removeAll()
-            guard let json = json else { print("error: no data recieved from mashape API Client"); return}
-            for object in json {
-                if let trailData = object.1.array {
-                    for i in 0..<trailData.count{
-                        self.mashapeData.append(MashapeData(json: trailData[i]))
-                        print("@@@@@@@@@@@@@@\(trailData[i])")
-                    }
-                }
-            }
-            completion()
-        }
-    }
-}
+
+
+//func getTrailsWithCompletion(completion: () -> ()) {
+//    MashapeAPIClient.getTrails { (json) in
+//        self.mashapeData.removeAll()
+//        guard let json = json else { print("error: no data recieved from mashape API Client"); return}
+//        for object in json {
+//            if let trailData = object.1.array {
+//                for i in 0..<trailData.count{
+//                    self.mashapeData.append(MashapeData(json: trailData[i]))
+//                    print("@@@@@@@@@@@@@@\(trailData[i])")
+//                }
+//            }
+//        }
+//        completion()
+//    }
+//}
+//}
 
 
 
