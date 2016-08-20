dde1241013059323fad69eba7d4142a8bc1d6b0e
Cordavi <michaelamundsen@gmail.com>
2016-08-20 09:32:16 -0400

    Refactored api call to pass along only the venue json data up the chain to simplify asych call

--- Anytrail/Anytrail/FoursquareAPIClient.swift
+++ Anytrail/Anytrail/FoursquareAPIClient.swift
@@ -9,49 +9,39 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
+import CoreLocation
+
+struct FoursquareConstants {
+    static let v = "20160808"
+    static let query = "HISTORIC/MONUMENTS/LANDMARKS/STATUES/MUSEUM/POI/Hiking trails"
+}
+
 
 class FoursquareAPIClient {
     
-    static let sharedInstance = FoursquareAPIClient()
-    
-    class func getQueryForSearchLandmarks(completion: (JSON?) -> ()){
-        let store = LocationDataStore.sharedInstance
+    class func getQueryForSearchLandmarks(parameter: [String: String], completion: [JSON]? -> ()) {
         
-        //        let coordinatesNESW:[Double] = store.settingRectangleForFoursquare()
-        //        let sw = "\(coordinatesNESW[2]),\(coordinatesNESW[3])"
-        //        let ne = "\(coordinatesNESW[0]),\(coordinatesNESW[1])"
-       
-        
-        let clientID = Keys.fourSquareClientID
-        let clientSecret = Keys.fourSquareClientSecret
-//        NSOperationQueue().addOperationWithBlock {
-        for latLong in store.returningLongLatArray(){
-            let origin = "\(Double(latLong.latitude)), \(Double(latLong.longitude))"
+        Alamofire.request(.GET, ATConstants.Endpoints.FOURSQUARE_GET_VENUES, parameters: parameter, headers: nil).responseJSON { response in
             
-            let v = "20160808"
-            let query = "HISTORIC/MONUMENTS/LANDMARKS/STATUES/MUSEUM/POI/Hiking trails"
-            let parameter = ["client_id": clientID,
-                             "client_secret":clientSecret,
-                             "v":v,
-                             "ll": origin,
-                             "query" : query,
-                             "radius" : String(store.determineRadius())]
-            //                         "ne" : ne,
-            //                         "sw": sw]
-//            print(parameter)
-            Alamofire.request(.GET, ATConstants.Endpoints.FOURSQUARE_GET_VENUES, parameters: parameter, headers: nil).responseJSON { (response) in
-                if let data = response.data {
-                    let jsonData = JSON(data: data)
-                    //                for venue in jsonData["response"]["groups"].array! {
-                    //                    for item in venue["items"] {
-                    //                        print(item.1.dictionary!["venue"]!["id"])
-                    //                        print(item.1.dictionary!["venue"]!["name"])
-                    //                    }
-                    //                }
-                   
-                 completion(jsonData)
-                }
+            guard let data = response.data where response.result.error == nil else {
+                print(response.result.error)
+                return
             }
+            
+            guard let responseJSON = JSON(data: data).dictionaryValue["response"]?.dictionaryValue else {
+                print("No venue's found")
+                return
+            }
+            
+            guard let groupsJSON = JSON(responseJSON).dictionaryValue["groups"]?.arrayValue else {
+                return
+            }
+            
+            guard let itemsJSON = JSON(groupsJSON).arrayValue.first?["items"].array else {
+                return
+            }
+            
+            completion(itemsJSON)
         }
     }
 }
@@ -60,16 +50,16 @@ class FoursquareAPIClient {
 //        class func getPhotoForVenue(completion: (String?) -> ()) {
 //            let clientID = Keys.fourSquareClientID
 //            let clientSecret = Keys.fourSquareClientSecret
-//            
+//
 //            let v = "20160808"
 //            let parameter = ["client_id"     : clientID,
 //                             "client_secret" :clientSecret,
 //                             "v"             : v,
 //                             "VENUE_ID"      : id]
-//            
-//            
 //
-//            
+//
+//
+//
 //            Alamofire.request(.GET, ATConstants.Endpoints.FOURSQUARE_GET_PHOTO, parameters: parameter, headers: nil).responseJSON { (response) in
 //                if let data = response.data {
 //                    let jsonData = JSON(data: data)
@@ -79,7 +69,7 @@ class FoursquareAPIClient {
 //                    //                        print(item.1.dictionary!["venue"]!["name"])
 //                    //                    }
 //                    //                }
-//                    
+//
 //                    //                completion(jsonData)
 //                }
 //            }
