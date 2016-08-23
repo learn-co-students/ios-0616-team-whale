cc2b2d8fc9e599a0d69cb6fcbe9507326528259d
ellscha <escharlin@wesleyan.edu>
2016-08-23 13:12:34 -0400

    the labels are working

--- Anytrail/Anytrail/UserProfileCell.swift
+++ Anytrail/Anytrail/UserProfileCell.swift
@@ -16,23 +16,14 @@ class UserProfileCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-        //dataLabel.text = "DATA"
-        print("awake from nib")
-        self.configureCell()
+        self.userCellBackgroundView.backgroundColor = ATConstants.Colors.GRAY
+
         dataLabel.textColor = UIColor(red: 80/225, green: 80/225, blue: 80/255, alpha: 1)
-        self.backgroundColor = UIColor.whiteColor()
     }
     
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
-        // Configure the view for the selected state
-    }
-    func configureCell(){
-        self.userCellBackgroundView?.layer.cornerRadius = 8.0
-        self.userCellBackgroundView.backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
-        self.dataLabel.backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
     }
     
     func giveCellData(dataIconStringName: UIImage, dataLabel: String) {
